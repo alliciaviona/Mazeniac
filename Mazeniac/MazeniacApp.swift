70c84647a2ef25a31a485619d13b165e7a8a50af
Allicia Viona <alliciawn@gmail.com>
2023-06-21 14:13:02 +0700

    Initial Commit

---
+++ Mazeniac/MazeniacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazeniacApp.swift
+//  Mazeniac
+//
+//  Created by Allicia Viona Sagi on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MazeniacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
